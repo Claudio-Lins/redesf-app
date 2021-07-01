ede66cebbaffd1d0c0462fda7abaff0608a28fc1
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-07-01 20:58:52 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <div className=" min-h-screen flex justify-center items-center bg-[#1da1f1] font-black text-[690px]">
-      <h1 className="text-red-500">Home</h1>
+    <div className=" min-h-screen flex justify-center items-center bg-[#1da1f1] font-black text-[90px]">
+      <h1 className="text-white">Home</h1>
     </div>
   )
 }
